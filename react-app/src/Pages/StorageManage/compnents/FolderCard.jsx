3e1c05893fbd35f55ed9e19919fdf5e2cfb8233f
lxfriday <liu3248184446@outlook.com>
2022-04-14 20:58:50 +0800

    update: 文件夹显示的时候去除尾部的 /

--- react-app/src/Pages/StorageManage/compnents/FolderCard.jsx
+++ react-app/src/Pages/StorageManage/compnents/FolderCard.jsx
@@ -3,6 +3,8 @@ import { FolderFilled, RollbackOutlined } from '@ant-design/icons'
 import styles from './FolderCard.module.less'
 
 export default function FolderCard({ folderName, handleClick, isBackward }) {
+  // 文件夹显示的时候去除尾部 /
+  const trimmedFolderName = folderName.slice(0, -1)
   return (
     <div
       className={styles.wrapper}
@@ -16,7 +18,7 @@ export default function FolderCard({ folderName, handleClick, isBackward }) {
           <FolderFilled style={{ color: '#aaa', fontSize: '60px' }} />
         )}
       </div>
-      <div className={styles.fileFullName}>{folderName}</div>
+      <div className={styles.fileFullName}>{trimmedFolderName}</div>
     </div>
   )
 }
